2a82ba888af3c4f9deb14dc2b3b205a206973820
sampod76 <108483839+sampod76@users.noreply.github.com>
2023-10-10 00:40:35 +0600

    add book interface file

---
+++ src/app/modules/Books/BooksInterface.ts
@@ -0,0 +1,12 @@
+export type IBookFilters = {
+    search?: string;
+    minPrice?: string;
+    maxPrice?: string;
+    category?: string;
+    title?: string;
+    price?: string;
+    genre?: string;
+    publicationDate?: string;
+    author?: string;
+  
+  };
