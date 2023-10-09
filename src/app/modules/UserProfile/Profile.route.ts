b04f227ed24d75136f0bbe60ac3e90f0af728963
sampod76 <108483839+sampod76@users.noreply.github.com>
2023-10-10 00:38:37 +0600

    add user profile router file

---
+++ src/app/modules/UserProfile/Profile.route.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
+import { ProfileController } from './ProfileController';
+
+const router = Router();
+router.get(
+  '/',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.CUSTOMER),
+  ProfileController.getProfileData
+);
+export const profileRoutes = router;
