2e30983010c26dfaf5d1d744714f4d1fc0534c50
sampod76 <108483839+sampod76@users.noreply.github.com>
2023-10-10 00:36:22 +0600

    add user validation file

---
+++ src/app/modules/Users/UsersValidation.ts
@@ -0,0 +1,15 @@
+import { z } from 'zod';
+const updateOneUser = z.object({
+  body: z.object({
+    name: z.string().optional(),
+    email: z.string().optional(),
+    password: z.string().optional(),
+    role: z.string().optional(),
+    contactNo: z.string().optional(),
+    address: z.string().optional(),
+    profileImg: z.string().optional(),
+    dateOfBirth: z.string().optional(),
+  }),
+});
+
+export const UsersValidation = { updateOneUser };
