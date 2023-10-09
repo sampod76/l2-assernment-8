1c86962fd9f8729f36a579dacaa23c3caa532ab0
sampod76 <108483839+sampod76@users.noreply.github.com>
2023-10-10 00:40:54 +0600

    add book valaditon file

---
+++ src/app/modules/Books/BooksValidation.ts
@@ -0,0 +1,38 @@
+
+import { z } from 'zod';
+const createBookV = z.object({
+  body: z.object({
+    title: z.string({
+      required_error: 'title is Required.',
+    }),
+    author: z.string({
+      required_error: 'author is Required.',
+    }),
+    genre: z.string({
+      required_error: 'genre is Required.',
+    }),
+    price: z.number({
+      required_error: 'price is Required.',
+    }),
+    publicationDate: z.string({
+      required_error: 'publicationDate is Required.',
+    }),
+    categoryId: z.string({
+      required_error: 'categoryId is Required.',
+    }),
+  }),
+});
+const updateOneBook = z.object({
+  body: z.object({
+    title: z.string().optional(),
+    author: z.string().optional(),
+    genre: z.string().optional(),
+    price: z.number().optional(),
+    publicationDate: z.string().optional(),
+    categoryId: z.string().optional(),
+  }),
+});
+
+export const BookValidation = { createBookV ,updateOneBook};
+
+
